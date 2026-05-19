426d9d2433fc88e79ba3766b6879db0a3589e61c
Torstein Krause Johansen <torstein@skybert.net>
2026-05-19 21:54:09 +0200

    Copy all files in the dir of the Markdown file

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -141,9 +142,71 @@ func (w Woo) getTargetFile(targetDir, fn string) (string, error) {
 // copyFilesToTargetDir copies non-markdown files in the same
 // directory as fn (fn, being the full path to a Markdown file) to
 // target dir.
-func (w Woo) copyFilesToTargetDir(fn string, targetDir string) {
-	srcDir := filepath.Dir(fn)
+func (w Woo) copyFilesToTargetDir(srcFileName string, targetDir string) error {
+	srcDir := filepath.Dir(srcFileName)
+	dfi, derr := os.Stat(targetDir)
+	if derr != nil {
+		return derr
+	}
+	err := filepath.WalkDir(
+		srcDir,
+		func(path string, d fs.DirEntry, err error) error {
+			if d.IsDir() {
+				return nil
+			}
+			sfi, err := os.Stat(path)
+			if err != nil {
+				return err
+			}
+			if !sfi.Mode().Perm().IsRegular() {
+				fmt.Printf("Not a regular file: %v\n", sfi)
+				return nil
+			}
+			if os.SameFile(dfi, sfi) {
+				return nil
+			}
+			srcFile, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer srcFile.Close()
+			targetFileName := targetDir +
+				string(filepath.Separator) +
+				filepath.Base(path)
+
+			dstFile, err := os.Create(targetFileName)
+			defer func() error {
+				if err := dstFile.Close(); err != nil {
+					return err
+				}
+				return nil
+			}()
+
+			if err != nil {
+				return err
+			}
+
+			bytes, err := io.Copy(dstFile, srcFile)
+			if err != nil {
+				return err
+			}
+			err = dstFile.Sync()
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf(
+				"Copied %v to %v, %v bytes in total\n",
+				srcFile.Name(),
+				dstFile.Name(),
+				bytes)
+
+			return nil
+		},
+	)
+
 	fmt.Printf("TODO Copy all files in %v to %v\n", srcDir, targetDir)
+	return err
 }
 
 func (w Woo) addHeaderAndFooter(s string, values map[string]string) (string, error) {
